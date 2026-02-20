d1b0be7a3d3774ad0707306192c43739cd973b54
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2026-02-20 16:43:39 +0500

    dfds

--- config/settings.py
+++ config/settings.py
@@ -170,13 +170,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'static/'
 
 if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static'
     ]
 else:
-    STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
