8cfec6226c413bcd9248e7ff9ee0b7bf8bb95588
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2026-02-20 16:56:29 +0500

    sd

--- config/settings.py
+++ config/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This is a list of additional directories where Django will look for static files 
 # during development (and for collectstatic to copy from).
